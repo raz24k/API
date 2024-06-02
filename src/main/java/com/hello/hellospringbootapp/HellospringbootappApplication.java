703a25a371c8b6c8cb743fa13879cd43ccd4fabd
raz24k <raz24k@gmail.com>
2024-06-02 16:53:42 +0530

    Initial commit of Spring Boot Hello World application

---
+++ src/main/java/com/hello/hellospringbootapp/HellospringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.hello.hellospringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringbootappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/hellospringbootapp/HellospringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.hellospringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
