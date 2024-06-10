de68e14d3ae0906dbd19b57062f86c36cf29c3d8
raz24k <161609831+raz24k@users.noreply.github.com>
2024-06-10 13:55:28 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,9 +2,9 @@
 echo "Running BeforeInstall script"
 
 # Check if the application is running
-if systemctl is-active --quiet hellospringbootapp; then
+if systemctl is-active --quiet springbootappcodedeploy; then
     echo "Stopping the running Spring Boot application"
-    sudo systemctl stop hellospringbootapp
+    sudo systemctl stop springbootappcodedeploy
 else
     echo "Spring Boot application is not running"
 fi
