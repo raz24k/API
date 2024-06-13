626eb14ed408950d1220188202e414493faf4e31
raz24k <161609831+raz24k@users.noreply.github.com>
2024-06-13 22:42:05 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Running AfterInstall script"
 # Add any post-installation steps, like changing file permissions
-sudo chmod +x /home/ubuntu/*.jar
+sudo chmod +Rf +x /home/ubuntu/
+sudo chown -Rf ubuntu:ubuntu /home/ubuntu/
