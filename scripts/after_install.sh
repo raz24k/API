8a587f6e7ab3d49341933b618a62edd99f3ce7a4
raz24k <161609831+raz24k@users.noreply.github.com>
2024-06-24 14:34:51 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Running AfterInstall script"
 # Add any post-installation steps, like changing file permissions
-sudo chmod +Rf +x /home/ubuntu/
-sudo chown -Rf ubuntu:ubuntu /home/ubuntu/
+sudo chmod +Rf +x /home/ubuntu/deploy/
+sudo chown -Rf ubuntu:ubuntu /home/ubuntu/deploy
